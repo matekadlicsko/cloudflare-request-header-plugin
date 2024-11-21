5bebf6767d166cc143c958078c86fc69859f470d
mate.kadlicsko <kadlicsko.mate@gmail.com>
2024-11-21 15:36:48 +0100

    Update cloudflare_request_header_provider.py

--- cloudflare_request_header_plugin/cloudflare_request_header_provider.py
+++ cloudflare_request_header_plugin/cloudflare_request_header_provider.py
@@ -14,6 +14,6 @@ class CloudflareRequestHeaderProvider(RequestHeaderProvider):
     def request_headers(self):
         request_headers = {}
         request_headers["cf-access-token"] = os.getenv("CF_ACCESS_TOKEN")
-        request_headers["CF-Access-Client-Id"] = os.getenv("CF_ACCESS_CLIENT_ID")
-        request_headers["CF-Access-Client-Secret"] = os.getenv("CF-Access-Client-Secret")
+        request_headers["cf-access-client-id"] = os.getenv("CF_ACCESS_CLIENT_ID")
+        request_headers["cf-access-client-secret"] = os.getenv("CF-ACCESS-CLIENT-SECRET")
         return request_headers
